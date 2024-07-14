41b3adbbfc5829713368f498cfcf9d781850d4fc
Evgeniy Lapshin <trytostopme2010@yandex.ru>
2024-07-14 21:25:53 +0300

    after fix

--- api_yamdb/auth_app/urls.py
+++ api_yamdb/auth_app/urls.py
@@ -6,7 +6,7 @@ app_name = 'auth_app'
 
 
 urlpatterns = [
-    path('v1/auth_app/signup/', SignupView.as_view(), name='user-signup'),
-    path('v1/auth_app/token/', CreateTokenView.as_view(),
+    path('v1/auth/signup/', SignupView.as_view(), name='user-signup'),
+    path('v1/auth/token/', CreateTokenView.as_view(),
          name='token-generation'),
 ]
