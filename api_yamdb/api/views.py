d74e768c9d0f96d657c2de2df90ad9c02a01d5f1
Evgeniy188 <80258394+Evgeniy188@users.noreply.github.com>
2024-07-22 07:56:18 +0000

    Fix after review-3

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,7 +2,7 @@ from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
 from rest_framework import (filters, generics, mixins, permissions,
-                            serializers, status, viewsets)
+                            status, viewsets)
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
